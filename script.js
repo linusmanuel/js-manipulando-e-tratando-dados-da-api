18dc8102a32393011835a2af8d71fe66130016c6
Joao Lino <linusmannuel@gmail.com>
2023-06-02 22:19:12 +0100

    featch api

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-/*
+const searchCEP = fetch('URL: viacep.com.br/ws/01001000/json/');
 
---- Código da conversa versão síncrono ---
-
-console.log("Mandando oi pro amigo!");
-
-function mandaMensagem() {
-    console.log("Tudo bem?");
-    console.log("Vou te mandar uma solicitação!");
-    console.log("Solicitação recebida!");
-}
-
-mandaMensagem();
-
-console.log("Tchau tchau!");
-
-*/
-
-console.log("Mandando oi pro amigo!");
-
-function mandaMensagem() {
-    console.log("Tudo bem?");
-    console.log("Vou te mandar uma solicitação!");
-    console.log("Solicitação recebida!");
-}
-
-setTimeout(mandaMensagem, 5000);
-
-console.log("Tchau tchau!");
+console.log(searchCEP);
