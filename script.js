fe984061070b46a8d5dfe31e75ec7445820a6065
Joao Lino <linusmannuel@gmail.com>
2023-06-05 17:57:07 +0100

    Tratando erros com catch

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-const searchCEP = fetch('https://viacep.com.br/ws/01001000/json/')
+const searchCEP = fetch('https://viacep.com.br/ws/01001250/json/')
 	.then((resposta) => resposta.json())
-	.then((r) => console.log(r));
+	.then((r) => {
+		if (r.erro) {
+			throw Error('Esse CEP é inválido!');
+		} else {
+			console.log(r);
+		}
+	})
+	.catch((erro) => console.log(erro));
 
 console.log(searchCEP);
