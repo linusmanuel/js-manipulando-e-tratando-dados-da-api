60862b08e52240a247558aa829b0d695c4ecebf5
Joao Lino <linusmannuel@gmail.com>
2023-06-08 18:55:44 +0100

    Async Await

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-const searchCEP = fetch('https://viacep.com.br/ws/01001250/json/')
-	.then((resposta) => resposta.json())
-	.then((r) => {
-		if (r.erro) {
-			throw Error('Esse CEP é inválido!');
-		} else {
-			console.log(r);
-		}
-	})
-	.catch((erro) => console.log(erro))
-	.finally((mensagem) => console.log('Finalmente terminou!'));
+async function searchAddress() {
+	const bringCEP = await fetch('https://viacep.com.br/ws/01001000/json');
+	const convertedCEP = await bringCEP.json();
+	console.log(convertedCEP);
+}
 
-console.log(searchCEP);
+searchAddress();
