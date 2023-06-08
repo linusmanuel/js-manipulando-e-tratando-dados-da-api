90fbfb01e3d2d0d548d03b01b0f4d1c772be28f9
Joao Lino <linusmannuel@gmail.com>
2023-06-08 20:46:47 +0100

    tratando erros

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 async function searchAddress() {
-	const bringCEP = await fetch('https://viacep.com.br/ws/01001000/json');
-	const convertedCEP = await bringCEP.json();
-	console.log(convertedCEP);
+	try {
+		const bringCEP = await fetch('https://viacep.com.br/ws/01001250/json');
+		const convertedCEP = await bringCEP.json();
+		if (convertedCEP.erro) {
+			throw Error('Erro CEP não encontrado');
+		}
+		console.log(convertedCEP);
+	} catch (erro) {
+		console.log(erro);
+	}
 }
 
 searchAddress();
