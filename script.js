ec31448ffae07d543403dd5cbc86273beb22af0b
Joao Lino <linusmannuel@gmail.com>
2023-06-08 22:04:26 +0100

    Promise All

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-async function searchAddress() {
+async function searchAddress(cep) {
 	try {
-		const bringCEP = await fetch('https://viacep.com.br/ws/01001250/json');
+		const bringCEP = await fetch(`https://viacep.com.br/ws/${cep}/json`);
 		const convertedCEP = await bringCEP.json();
 		if (convertedCEP.erro) {
 			throw Error('Erro CEP não encontrado');
 		}
 		console.log(convertedCEP);
+		return convertedCEP;
 	} catch (erro) {
 		console.log(erro);
 	}
 }
 
-searchAddress();
+const ceps = ['01001000', '01001001'];
+let listOfCEPS = ceps.map((values) => searchAddress(values));
+console.log(listOfCEPS);
+Promise.all(listOfCEPS).then((res) => console.log(res));
