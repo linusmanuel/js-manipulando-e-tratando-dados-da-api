4a1fbf98405df35e3f6d34cd73137404d92de70b
Joao Lino <linusmannuel@gmail.com>
2023-06-09 23:40:30 +0100

    evitando problemas

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 const cidade = document.querySelector('#cidade');
 const logradouro = document.querySelector('#endereco');
 const estado = document.querySelector('#estado');
+const complemento = document.querySelector('#complemento');
+const bairro = document.querySelector('#bairro');
+const messageErro = document.querySelector('#messageErro');
 
 async function searchAddress(cep) {
+	messageErro.innerHTML = '';
 	try {
 		const bringCEP = await fetch(`https://viacep.com.br/ws/${cep}/json`);
 		const convertedCEP = await bringCEP.json();
 		cidade.value = convertedCEP.localidade;
 		logradouro.value = convertedCEP.logradouro;
 		estado.value = convertedCEP.uf;
+		complemento.value = convertedCEP.complemento;
+		bairro.value = convertedCEP.bairro;
 		if (convertedCEP.erro) {
 			throw Error('Erro CEP não encontrado');
 		}
-		console.log(convertedCEP);
 		return convertedCEP;
 	} catch (erro) {
-		console.log(erro);
+		messageErro.innerHTML = `<p>CEP inválido, tente novamente!</p>`;
 	}
 }
 
